60b61aaeadf452dfac66fa6df9ff55a2078729ad
z5n <dev@antidep.xyz>
2021-03-08 12:40:07 -0800

    pushed 1615236007

---
+++ faq/index.php
@@ -0,0 +1,111 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+		
+		
+	
+	
+
+		
+<!DOCTYPE html>>
+
+		<title>Moshi Classic - FAQ</title>
+		<meta name="Author" content="Mind Candy">
+		<meta name="Description" content="Moshi Monsters is a brand new online game where you can adopt your very own pet monster. Each one has a lively, unique personality that develops the more you play.">
+		<meta name="Keywords" content="playmoshi, play moshi, playmoshi.com, education, social networking, virtual world, virtual pet, puzzles, monsters, furi, diavlo, zommer, poppet, luvli, katsuma">
+		<meta http-equiv="pragma" content="no-cache">
+		<meta http-equiv="cache-control" content="no-cache">
+		<meta http-equiv="expires" content="-1"> 
+		<meta http-equiv="content-type" content="text/html; charset=utf-8">
+		<link rel="shortcut icon" href="/assets/images/favicon.png" type="image/x-icon">
+		<!-- SWFObject embed by Geoff Stearns geoff@deconcept.com http://blog.deconcept.com/swfobject/ -->
+	 	<script src="/assets/js/swfobject.js" type="text/javascript"></script>
+	 	<script src="/assets/js/diablo.js" type="text/javascript"></script>	 	 	
+		<script language="javascript1.2" src="/assets/js/engine.js" type="text/javascript"></script> 
+			
+		
+			
+		<link rel="stylesheet" type="text/css" href="/assets/css/core.css" media="screen">
+			
+			
+		
+	 	<link rel="stylesheet" type="text/css" href="/assets/css/print.css" media="print">
+	
+	</head>
+	
+
+
+		<div id="wrapper">
+			
+		<!-- Branding -->	
+			
+		<div id="header">
+		<a href = "/" target = "_self"> 
+		<img src="/assets/images/logo.jpg">
+      </a></div>
+			<a class="hide" href="#navbar">Skip to navigation &#8594;</a>
+			<hr class="hide">
+			
+		<!-- /Branding -->
+	
+			<!-- Main Content -->
+			<div id="main-wrapper">
+				<div id="main-header">
+				
+				</div>
+				<div id="main-copy">
+				<!-- Sub Navigation Bar -->
+				  <div id="main-nav">
+		    	  	
+<div class="clear"></div>
+				  </div>
+				  <!-- /Sub Navigation Bar -->
+				  <!-- Copy Starts Here -->
+				  
+					<div class="inner">
+						<h2>Frequently Asked Questions</h2><br>
+						<div class="faq-questions">
+							<div class="questions-inner">
+							<?php include('../assets/core/questions.html') ?>
+							</div>
+							<div class="questions-footer"></div>
+						</div>
+
+						<div class="faq-answers">
+							<div class="answers-inner">
+							<?php include('../assets/core/answers.html') ?>
+								<p><a href="#header">Back to top</a></p> <br>
+							</div>
+						</div>
+						<div class="clear"></div>
+					</div>
+					
+				</div>
+				<!-- Copy Ends Here -->
+				
+				<div id="main-footer"></div>
+				</div>
+		<?php include('../assets/core/nav.html') ?>
+				
+			</div>
+			<?php include('../assets/core/footer.html') ?>
+		
+		
+		
+		</body>
+		
+		</html>
---
+++ login/index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+		<title>Moshi Classic - Sign In</title>
+		<meta name="Author" content="Mind Candy">
+		<meta name="Description" content="Moshi Monsters is a brand new online game where you can adopt your very own pet monster. Each one has a lively, unique personality that develops the more you play.">
+		<meta name="Keywords" content="playmoshi, play moshi, playmoshi.com, education, social networking, virtual world, virtual pet, puzzles, monsters, furi, diavlo, zommer, poppet, luvli, katsuma">
+		<meta http-equiv="pragma" content="no-cache">
+		<meta http-equiv="cache-control" content="no-cache">
+		<meta http-equiv="expires" content="-1"> 
+		<meta http-equiv="content-type" content="text/html; charset=utf-8">
+		<link rel="shortcut icon" href="/assets/images/favicon.png" type="image/x-icon">
+	 	<script src="/assets/js/swfobject.js" type="text/javascript"></script>
+	 	<script src="/assets/js/diablo.js" type="text/javascript"></script>	 	 	
+		<script language="javascript1.2" src="/assets/js/engine.js" type="text/javascript"></script> 
+			
+		
+			
+		<link rel="stylesheet" type="text/css" href="/assets/css/core.css" media="screen">
+			
+			
+		
+	 	<link rel="stylesheet" type="text/css" href="/assets/css/print.css" media="print">
+	 	
+	 	
+			
+	</head>
+	
+	
+		
+
+	
+
+		<div id="wrapper">
+			
+		<!-- Branding -->	
+			
+		<div id="header">
+		<a href = "/" target = "_self"> 
+		<img src="/assets/images/logo.jpg">
+      </a></div>
+			<a class="hide" href="#navbar">Skip to navigation &#8594;</a>
+			<hr class="hide">
+			
+		<!-- /Branding -->
+
+			<!-- Main Content -->
+			<div id="main-wrapper">
+				<div id="main-header">
+					
+				</div>
+				<div id="main-copy">
+				  <!-- Copy Starts Here -->
+				  
+					<div class="inner">
+						<h2>Sign In</h2>
+					
+						<div class="col" id="col-spacer">
+						
+							<h4>Enter your login details below</h4>
+							
+							<form id="login" name="login" onsubmit="return true;" action="/web/20110615140159/http://www.moshimonsters.com/login/retry" method="post">
+								<input type="hidden" name="target" value>
+								<dl>
+									<dt>
+										<label for="login_username">Owner Name</label>
+									</dt>
+									<dd>
+										<input class="line-input" name="username" value id="login_username" type="text">
+										
+									</dd>
+									<dt>
+										<label for="login_password">Password</label>
+									</dt>
+									<dd>
+										<input class="line-input" name="password" id="login_password" type="password">
+
+									</dd>
+									<br>
+									<p><input class="btn" id="login-btn" type="submit" value="Login"></p>
+															</form>
+							<h5><a href="/web/20110615140159/http://www.moshimonsters.com/forgotpassword">Reset my password</a> - If you have forgotten your password, then we can send you an email that will let you reset it.</h5>
+						</div>
+						
+						<div class="col">
+							
+							<p class="register-adopt">Don't have a monster? <a href="/web/20110615140159/http://www.moshimonsters.com/adopt">Why not adopt?</a></p>
+						
+						</div>
+						<div class="clear"></div>
+				
+					</div>
+					
+				</div>				
+				<div id="main-footer"></div>
+			</div>
+	<?php include('../assets/core/nav.html') ?>
+			
+		</div>
+		<?php include('../assets/core/footer.html') ?>
+
+	
+
+	</body>
+	
+</html>
