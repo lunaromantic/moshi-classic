cd41bc6bcd4d16542a6db78e47ad369bc1762955
z5n <dev@antidep.xyz>
2021-03-08 13:26:22 -0800

    pushed 1615238782

--- about/index.php
+++ about/index.php
@@ -1,72 +1,25 @@
 <!DOCTYPE html>
-	<head>
-		<title>Moshi Classic - About</title>
-		<meta name="Author" content="Mind Candy">
-		<meta name="Description" content="Moshi Monsters is a brand new online game where you can adopt your very own pet monster. Each one has a lively, unique personality that develops the more you play.">
-		<meta name="Keywords" content="playmoshi, play moshi, playmoshi.com, education, social networking, virtual world, virtual pet, puzzles, monsters, furi, diavlo, zommer, poppet, luvli, katsuma">
-		<meta http-equiv="pragma" content="no-cache">
-		<meta http-equiv="cache-control" content="no-cache">
-		<meta http-equiv="expires" content="-1"> 
-		<meta http-equiv="content-type" content="text/html; charset=utf-8">
-		<link rel="shortcut icon" href="/assets/images/favicon.png" type="image/x-icon">
-	 	<script src="/assets/js/swfobject.js" type="text/javascript"></script>
-	 	<script src="/assets/js/diablo.js" type="text/javascript"></script>	 	 	
-		<script language="javascript1.2" src="/assets/js/engine.js" type="text/javascript"></script> 
-			
-		
-			
-		<link rel="stylesheet" type="text/css" href="/assets/css/core.css" media="screen">		
-	 	<link rel="stylesheet" type="text/css" href="/assets/css/print.css" media="print">
-	 	
-	 	
-			
-	 	
-	</head>
-	
-		
-	
+<title>Moshi Classic - About</title>
+<?php include('../assets/core/requirements.html') ?>
 
-		<div id="wrapper">
-			
-		<div id="header">
-		<a href = "/" target = "_self"> 
-		<img src="/assets/images/logo.jpg">
-      </a></div>
-			<a class="hide" href="#navbar">Skip to navigation &#8594;</a>
-			<hr class="hide">
-			
+<body>
+    <?php include('../assets/core/nav1.html') ?>
+    <div id="main-copy">
+        <div class="inner">
+            <h2>What is this project?</h2>
+            <center><br><br><br><br>
+                <h4>We all did enjoy Moshi Monsters back in the day. But unfortunately Mind Candy bacame a corporate company and watered down the site to hell. But we might bring back the Classic style and design of this game in HTML5.<br><br><br>
+                    <img src="/assets/images/sad6.jpg" width="100"><img src="/assets/images/sad1.jpg" width="100"><img src="/assets/images/sad2.jpg" width="100"><img src="/assets/images/sad4.jpg" width="100">
+                    <br><br><br><br></h4>
+			</center>
+                    <div class="clear"></div>
+        </div>
+    </div>
+    <div id="main-footer"></div>
+    </div>
+    <?php include('../assets/core/nav.html') ?>
+    </div>
+    <?php include('../assets/core/footer.html') ?>
+</body>
 
-			<div id="main-wrapper">
-			
-				<div id="main-header"></div>
-				
-				<div id="main-copy">
-				  
-					<div class="inner">
-						<h2>What is this project?</h2>
-					
-<center>
-<br>	<br>	<br>	<br>	
-								<h4>We all did enjoy Moshi Monsters back in the day. But unfortunately Mind Candy bacame a corporate company and watered down the site to hell. But we might bring back the Classic
-							style and design of this game in HTML5. 
-							<br>	<br>	<br>
-						
-							<img src="/assets/images/sad6.jpg" width="100">
-							<img src="/assets/images/sad1.jpg" width="100"><img src="/assets/images/sad2.jpg" width="100"><img src="/assets/images/sad4.jpg" width="100">
-								<br>	<br>	<br>	<br>
-</h4>
-							<center>
-						
-						<div class="clear"></div>
-						
-					</div>
-				</div>
-				
-				<div id="main-footer"></div>
-			</div>
-<?php include('../assets/core/nav.html') ?>
-</div>
-<?php include('../assets/core/footer.html') ?>
-	</body>
-	
 </html>
